3b9cf81866f64df74397c74a5d4f3824beceaaeb
Unknown <anthonyrc@immersionrc.com>
2020-09-29 07:17:55 +0200

    Add IRC Ghost Serial Rx Protocol
    Api version 1.44 and later

--- src/js/fc.js
+++ src/js/fc.js
@@ -545,6 +545,10 @@ const FC = {
                     result.push('SPEKTRUM SRXL2');
                 }
 
+                if (semver.gte(apiVersion, "1.44.0")) {
+                    result.push('IRC GHOST');
+                }
+
                 return result;
             },
         };
