6be6a30a5ba59c1f4720ce1adc2ac480669b7081
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-03-18 00:06:55 +0000

    RTFL: Clean up Setup tab

--- src/js/tabs/setup.js
+++ src/js/tabs/setup.js
@@ -12,18 +12,14 @@ TABS.setup.initialize = function (callback) {
     }
 
     function load_status() {
-        MSP.send_message(MSPCodes.MSP_STATUS, false, false, load_mixer_config);
-    }
-
-    function load_mixer_config() {
-        MSP.send_message(MSPCodes.MSP_MIXER_CONFIG, false, false, load_html);
+        MSP.send_message(MSPCodes.MSP_STATUS, false, false, load_html);
     }
 
     function load_html() {
         $('#content').load("./tabs/setup.html", process_html);
     }
 
-    MSP.send_message(MSPCodes.MSP_ACC_TRIM, false, false, load_status);
+    load_status();
 
     function process_html() {
         // translate to user-selected language
@@ -43,16 +39,6 @@ TABS.setup.initialize = function (callback) {
             backupButton.addClass('disabled');
         }
 
-        // initialize 3D Model
-        self.initModel();
-
-        // set roll in interactive block
-        $('span.roll').text(i18n.getMessage('initialSetupAttitude', [0]));
-        // set pitch in interactive block
-        $('span.pitch').text(i18n.getMessage('initialSetupAttitude', [0]));
-        // set heading in interactive block
-        $('span.heading').text(i18n.getMessage('initialSetupAttitude', [0]));
-
         // check if we have accelerometer and magnetometer
         if (!have_sensor(FC.CONFIG.activeSensors, 'acc')) {
             $('a.calibrateAccel').addClass('disabled');
@@ -63,10 +49,6 @@ TABS.setup.initialize = function (callback) {
             $('a.calibrateMag').addClass('disabled');
             $('default_btn').addClass('disabled');
         }
-
-        self.initializeInstruments();
-
-        $('#arming-disable-flag').attr('title', i18n.getMessage('initialSetupArmingDisableFlagsTooltip'));
 
         if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_40)) {
             if (isExpertModeEnabled()) {
@@ -153,17 +135,6 @@ TABS.setup.initialize = function (callback) {
             });
         });
 
-        // display current yaw fix value (important during tab re-initialization)
-        $('div#interactive_block > a.reset').text(i18n.getMessage('initialSetupButtonResetZaxisValue', [self.yaw_fix]));
-
-        // reset yaw button hook
-        $('div#interactive_block > a.reset').click(function () {
-            self.yaw_fix = FC.SENSOR_DATA.kinematics[2] * - 1.0;
-            $(this).text(i18n.getMessage('initialSetupButtonResetZaxisValue', [self.yaw_fix]));
-
-            console.log(`YAW reset to 0 deg, fix: ${self.yaw_fix} deg`);
-        });
-
         backupButton.click(function () {
             if ($(this).hasClass('disabled')) {
                 return;
@@ -187,139 +158,6 @@ TABS.setup.initialize = function (callback) {
             });
         });
 
-        // cached elements
-        const bat_voltage_e = $('.bat-voltage'),
-            bat_mah_drawn_e = $('.bat-mah-drawn'),
-            bat_mah_drawing_e = $('.bat-mah-drawing'),
-            rssi_e = $('.rssi'),
-            arming_disable_flags_e = $('.arming-disable-flags'),
-            gpsFix_e = $('.gpsFix'),
-            gpsSats_e = $('.gpsSats'),
-            gpsLat_e = $('.gpsLat'),
-            gpsLon_e = $('.gpsLon'),
-            roll_e = $('dd.roll'),
-            pitch_e = $('dd.pitch'),
-            heading_e = $('dd.heading');
-
-        if (semver.lt(FC.CONFIG.apiVersion, API_VERSION_1_36)) {
-            arming_disable_flags_e.hide();
-        }
-
-        // DISARM FLAGS
-        // We add all the arming/disarming flags available, and show/hide them if needed.
-        const prepareDisarmFlags = function() {
-
-            let disarmFlagElements = [
-                'NO_GYRO',
-                'FAILSAFE',
-                'RX_FAILSAFE',
-                'BAD_RX_RECOVERY',
-                'BOXFAILSAFE',
-                'THROTTLE',
-                'ANGLE',
-                'BOOT_GRACE_TIME',
-                'NOPREARM',
-                'LOAD',
-                'CALIBRATING',
-                'CLI',
-                'CMS_MENU',
-                'OSD_MENU',
-                'BST',
-                'MSP',
-            ];
-
-            if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_38)) {
-                disarmFlagElements.splice(disarmFlagElements.indexOf('THROTTLE'), 0, 'RUNAWAY_TAKEOFF');
-            }
-
-            if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_39)) {
-                disarmFlagElements = disarmFlagElements.concat(['PARALYZE',
-                                                                'GPS']);
-            }
-
-            if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_41)) {
-                disarmFlagElements.splice(disarmFlagElements.indexOf('OSD_MENU'), 1);
-                disarmFlagElements = disarmFlagElements.concat(['RESC']);
-                disarmFlagElements = disarmFlagElements.concat(['RPMFILTER']);
-            }
-            if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_42)) {
-                disarmFlagElements.splice(disarmFlagElements.indexOf('THROTTLE'), 0, 'CRASH');
-                disarmFlagElements = disarmFlagElements.concat(['REBOOT_REQD',
-                                                                'DSHOT_BBANG']);
-            }
-            if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_43)) {
-                disarmFlagElements = disarmFlagElements.concat(['NO_ACC_CAL', 'MOTOR_PROTO']);
-            }
-
-            // Always the latest element
-            disarmFlagElements = disarmFlagElements.concat(['ARM_SWITCH']);
-
-            // Arming allowed flag
-            arming_disable_flags_e.append('<span id="initialSetupArmingAllowed" i18n="initialSetupArmingAllowed" style="display: none;"></span>');
-
-            // Arming disabled flags
-            for (let i = 0; i < FC.CONFIG.armingDisableCount; i++) {
-
-                // All the known elements but the ARM_SWITCH (it must be always the last element)
-                if (i < disarmFlagElements.length - 1) {
-                    arming_disable_flags_e.append('<span id="initialSetupArmingDisableFlags' + i + '" class="cf_tip disarm-flag" title="' + i18n.getMessage('initialSetupArmingDisableFlagsTooltip' + disarmFlagElements[i]) + '" style="display: none;">' + disarmFlagElements[i] + '</span>');
-
-                // The ARM_SWITCH, always the last element
-                } else if (i == FC.CONFIG.armingDisableCount - 1) {
-                    arming_disable_flags_e.append('<span id="initialSetupArmingDisableFlags' + i + '" class="cf_tip disarm-flag" title="' + i18n.getMessage('initialSetupArmingDisableFlagsTooltipARM_SWITCH') + '" style="display: none;">ARM_SWITCH</span>');
-
-                // Unknown disarm flags
-                } else {
-                    arming_disable_flags_e.append('<span id="initialSetupArmingDisableFlags' + i + '" class="disarm-flag" style="display: none;">' + (i + 1) + '</span>');
-                }
-            }
-        };
-
-        prepareDisarmFlags();
-
-        function get_slow_data() {
-
-            MSP.send_message(MSPCodes.MSP_STATUS, false, false, function() {
-
-                $('#initialSetupArmingAllowed').toggle(FC.CONFIG.armingDisableFlags == 0);
-
-                for (let i = 0; i < FC.CONFIG.armingDisableCount; i++) {
-                    $('#initialSetupArmingDisableFlags'+i).css('display',(FC.CONFIG.armingDisableFlags & (1 << i)) == 0 ? 'none':'inline-block');
-                }
-
-            });
-
-            MSP.send_message(MSPCodes.MSP_ANALOG, false, false, function () {
-                bat_voltage_e.text(i18n.getMessage('initialSetupBatteryValue', [FC.ANALOG.voltage]));
-                bat_mah_drawn_e.text(i18n.getMessage('initialSetupBatteryMahValue', [FC.ANALOG.mAhdrawn]));
-                bat_mah_drawing_e.text(i18n.getMessage('initialSetupBatteryAValue', [FC.ANALOG.amperage.toFixed(2)]));
-                rssi_e.text(i18n.getMessage('initialSetupRSSIValue', [((FC.ANALOG.rssi / 1023) * 100).toFixed(0)]));
-            });
-
-            if (have_sensor(FC.CONFIG.activeSensors, 'gps')) {
-                MSP.send_message(MSPCodes.MSP_RAW_GPS, false, false, function () {
-                    gpsFix_e.html((FC.GPS_DATA.fix) ? i18n.getMessage('gpsFixTrue') : i18n.getMessage('gpsFixFalse'));
-                    gpsSats_e.text(FC.GPS_DATA.numSat);
-                    gpsLat_e.text((FC.GPS_DATA.lat / 10000000).toFixed(4) + ' deg');
-                    gpsLon_e.text((FC.GPS_DATA.lon / 10000000).toFixed(4) + ' deg');
-                });
-            }
-        }
-
-        function get_fast_data() {
-            MSP.send_message(MSPCodes.MSP_ATTITUDE, false, false, function () {
-                roll_e.text(i18n.getMessage('initialSetupAttitude', [FC.SENSOR_DATA.kinematics[0]]));
-                pitch_e.text(i18n.getMessage('initialSetupAttitude', [FC.SENSOR_DATA.kinematics[1]]));
-                heading_e.text(i18n.getMessage('initialSetupAttitude', [FC.SENSOR_DATA.kinematics[2]]));
-
-                self.renderModel();
-                self.updateInstruments();
-            });
-        }
-
-        GUI.interval_add('setup_data_pull_fast', get_fast_data, 33, true); // 30 fps
-        GUI.interval_add('setup_data_pull_slow', get_slow_data, 250, true); // 4 fps
-
         $('a.rebootMsc').click(function () {
             const buffer = [];
             if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_41) &&
@@ -337,37 +175,6 @@ TABS.setup.initialize = function (callback) {
     }
 };
 
-TABS.setup.initializeInstruments = function() {
-    const options = {size:90, showBox : false, img_directory: 'images/flightindicators/'};
-    const attitude = $.flightIndicator('#attitude', 'attitude', options);
-    const heading = $.flightIndicator('#heading', 'heading', options);
-
-    this.updateInstruments = function() {
-        attitude.setRoll(FC.SENSOR_DATA.kinematics[0]);
-        attitude.setPitch(FC.SENSOR_DATA.kinematics[1]);
-        heading.setHeading(FC.SENSOR_DATA.kinematics[2]);
-    };
-};
-
-TABS.setup.initModel = function () {
-    this.model = new Model($('.model-and-info #canvas_wrapper'), $('.model-and-info #canvas'));
-
-    $(window).on('resize', $.proxy(this.model.resize, this.model));
-};
-
-TABS.setup.renderModel = function () {
-    const x = (FC.SENSOR_DATA.kinematics[1] * -1.0) * 0.017453292519943295,
-        y = ((FC.SENSOR_DATA.kinematics[2] * -1.0) - this.yaw_fix) * 0.017453292519943295,
-        z = (FC.SENSOR_DATA.kinematics[0] * -1.0) * 0.017453292519943295;
-
-    this.model.rotateTo(x, y, z);
-};
-
 TABS.setup.cleanup = function (callback) {
-    if (this.model) {
-        $(window).off('resize', $.proxy(this.model.resize, this.model));
-        this.model.dispose();
-    }
-
     if (callback) callback();
 };
