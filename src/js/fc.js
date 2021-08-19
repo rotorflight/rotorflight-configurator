6297ec4445ab9aaa0834ebfdcebd08150266f8a2
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-08-19 17:19:13 +0100

    WIP: Remove runawayTakeoffPrevention

--- src/js/fc.js
+++ src/js/fc.js
@@ -25,7 +25,6 @@ const INITIAL_CONFIG = {
     armingDisableCount:               0,
     armingDisableFlags:               0,
     armingDisabled:                   false,
-    runawayTakeoffPreventionDisabled: false,
     boardIdentifier:                  "",
     boardVersion:                     0,
     targetCapabilities:               0,
