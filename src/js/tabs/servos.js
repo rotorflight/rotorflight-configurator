6af282bcb5e7271fda48aa3db4e4194cb1b00c93
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-08-20 01:46:21 +0100

    RTFL: Servo refactoring

--- src/js/tabs/servos.js
+++ src/js/tabs/servos.js
@@ -1,19 +1,23 @@
 'use strict';
 
-TABS.servos = {};
+TABS.servos = {
+    MAX_SERVOS: 8,
+    OVERRIDE_OFF: 2001,
+};
+
 TABS.servos.initialize = function (callback) {
+
+    const self = this;
 
     if (GUI.active_tab !== 'servos') {
         GUI.active_tab = 'servos';
     }
 
-    MSP.promise(MSPCodes.MSP_STATUS).then(function() {
-        return MSP.promise(MSPCodes.MSP_SERVO_CONFIGURATIONS);
-    }).then(function() {
-        return MSP.promise(MSPCodes.MSP_SERVO);
-    }).then(function() {
-        load_html();
-    });
+    MSP.promise(MSPCodes.MSP_STATUS)
+        .then(() => MSP.promise(MSPCodes.MSP_SERVO_CONFIGURATIONS))
+        .then(() => MSP.promise(MSPCodes.MSP_SERVO_OVERRIDE))
+        .then(() => MSP.promise(MSPCodes.MSP_SERVO))
+        .then(() => load_html());
 
     function load_html() {
         $('#content').load("./tabs/servos.html", process_html);
@@ -21,74 +25,136 @@ TABS.servos.initialize = function (callback) {
 
     function process_html() {
 
-        function process_servo(index) {
+        function process_override(servoIndex) {
 
-            const SERVO = FC.SERVO_CONFIG[index];
-            const rated = (SERVO.rate >= 0) ? SERVO.rate : -SERVO.rate;
-            const check = (SERVO.rate >= 0) ? '' : ' checked';
+            const servoOverride = $('#tab-servos-templates .servoOverrideTemplate tr').clone();
+            const servoSlider = servoOverride.find('.servoOverrideSlider');
+            const servoEnable = servoOverride.find('.servoOverrideEnable input');
+            const servoInput  = servoOverride.find('.servoOverrideInput input');
 
-            let element = `<tr class="servo${index}">`;
-            element += `<td>#${index+1}</td>`;
-            element += `<td><input id="mid" type="number" min="375" max="2250" value="${SERVO.mid}" /></td>`;
-            element += `<td><input id="min" type="number" min="-1000" max="1000" value="${SERVO.min}" /></td>`;
-            element += `<td><input id="max" type="number" min="-1000" max="1000" value="${SERVO.max}" /></td>`;
-            element += `<td><input id="rate" type="number" min="0" max="2500" value="${rated}" /></td>`;
-            element += `<td><input id="trim" type="number" min="-250" max="250" value="${SERVO.trim}" /></td>`;
-            element += `<td><input id="speed" type="number" min="0" max="10000" value="${SERVO.speed}" /></td>`;
-            element += `<td><input id="reversed" type="checkbox" class="toggle" ${check}/></td>`;
-            element += `<td><div class="meter">`;
-            element += `<div class="meter-bar">`;
-            element += `<div class="meter-label"></div>`;
-            element += `<div class="meter-fill">`;
-            element += `<div class="meter-label"></div>`;
-            element += `</div>`;
-            element += `</div>`;
-            element += `</div></td>`;
-            element += `</tr>`;
+            servoOverride.attr('class', `servoOverride${servoIndex}`);
+            servoOverride.find('.servoOverrideIndex').text(`#${servoIndex+1}`);
 
-            $('.tab-servos table.fields').append(element);
-            $('.tab-servos table.fields tr:last').data('info', {'index': index});
-            $('.tab-servos table.fields tr:last input').each(function () {
-                $(this).data('info', {'index': index});
+            servoSlider.noUiSlider({
+                range: {
+                    'min': -90,
+                    'max':  90,
+                },
+                start: 0,
+                step: 5,
+                behaviour: 'snap-drag',
             });
+
+            servoOverride.find('.pips-range').noUiSlider_pips({
+                mode: 'values',
+                values: [ -80, -60, -40, -20, 0, 20, 40, 60, 80, ],
+                density: 2.7777777777,
+                stepped: true,
+                format: wNumb({
+                    decimals: 0,
+                }),
+            });
+
+            servoSlider.on('slide', function () {
+                servoInput.val(parseInt($(this).val()));
+            });
+
+            servoSlider.on('change', function () {
+                servoInput.change();
+            });
+
+            servoInput.change(function () {
+                const value = $(this).val();
+                servoSlider.val(value);
+                FC.SERVO_OVERRIDE[servoIndex] = Math.round(value * 1000 / 60);
+                mspHelper.sendServoOverride(servoIndex);
+            });
+
+            servoEnable.change(function () {
+                const check = $(this).prop('checked');
+                const value = check ? 0 : self.OVERRIDE_OFF;
+
+                servoInput.val(0);
+                servoSlider.val(0);
+                servoInput.prop('disabled', !check);
+                servoSlider.attr('disabled', !check);
+
+                FC.SERVO_OVERRIDE[servoIndex] = value;
+                mspHelper.sendServoOverride(servoIndex);
+            });
+
+            const value = FC.SERVO_OVERRIDE[servoIndex];
+            const check = (value >= -2000 && value <= 2000);
+            const angle = check ? Math.round(value * 60 / 1000) : 0;
+
+            servoInput.val(angle);
+            servoSlider.val(angle);
+
+            servoInput.prop('disabled', !check);
+            servoSlider.attr('disabled', !check);
+            servoEnable.prop('checked', check);
+
+            $('.servoOverride tbody').append(servoOverride);
         }
 
-        function update_servo_config(index) {
+        function process_config(servoIndex) {
 
-            const servo = $(`.tab-servos .servo${index}`);
+            const servoConfig = $('#tab-servos-templates .servoConfigTemplate tr').clone();
 
-            let dir = ($('#reversed',servo).is(':checked')) ? -1 : 1;
+            const SERVO = FC.SERVO_CONFIG[servoIndex];
+            const rate = Math.abs(SERVO.rate);
+            const revs = (SERVO.rate < 0);
 
-            FC.SERVO_CONFIG[index].mid = parseInt($('#mid',servo).val());
-            FC.SERVO_CONFIG[index].min = parseInt($('#min',servo).val());
-            FC.SERVO_CONFIG[index].max = parseInt($('#max',servo).val());
-            FC.SERVO_CONFIG[index].rate = parseInt($('#rate',servo).val()) * dir;
-            FC.SERVO_CONFIG[index].trim = parseInt($('#trim',servo).val());
-            FC.SERVO_CONFIG[index].speed = parseInt($('#speed',servo).val());
+            servoConfig.attr('class', `servoConfig${servoIndex}`);
+            servoConfig.data('index', servoIndex);
+
+            servoConfig.find('#index').text(`#${servoIndex+1}`);
+            servoConfig.find('#mid').val(SERVO.mid);
+            servoConfig.find('#min').val(SERVO.min);
+            servoConfig.find('#max').val(SERVO.max);
+            servoConfig.find('#rate').val(rate);
+            servoConfig.find('#trim').val(SERVO.trim);
+            servoConfig.find('#speed').val(SERVO.speed);
+            servoConfig.find('#reversed').prop('checked', revs);
+
+            servoConfig.find('#reversed').change(function() {
+                const input = servoConfig.find('#trim');
+                input.val(-parseInt(input.val()));
+            });
+
+            servoConfig.find('input').change(function () {
+                update_servo_config(servoIndex);
+                mspHelper.sendServoConfig(servoIndex);
+            });
+
+            $('.servoConfig tbody').append(servoConfig);
         }
 
-        function update_servo_configurations(to_eeprom) {
-            $('.tab-servos table.fields tr:not(".header")').each(function () {
-                const index = $(this).data('info').index;
+        function update_servo_config(servoIndex) {
+
+            const servo = $(`.tab-servos .servoConfig${servoIndex}`);
+            const direc = $('#reversed',servo).prop('checked') ? -1 : 1;
+
+            FC.SERVO_CONFIG[servoIndex].mid = parseInt($('#mid',servo).val());
+            FC.SERVO_CONFIG[servoIndex].min = parseInt($('#min',servo).val());
+            FC.SERVO_CONFIG[servoIndex].max = parseInt($('#max',servo).val());
+            FC.SERVO_CONFIG[servoIndex].rate = parseInt($('#rate',servo).val()) * direc;
+            FC.SERVO_CONFIG[servoIndex].trim = parseInt($('#trim',servo).val());
+            FC.SERVO_CONFIG[servoIndex].speed = parseInt($('#speed',servo).val());
+        }
+
+        function save_servo_configuration() {
+
+            for (let index = 0; index < self.MAX_SERVOS; index++) {
                 update_servo_config(index);
-            });
-
-            mspHelper.sendServoConfigurations(save_to_eeprom);
-
-            function save_to_eeprom() {
-                if (to_eeprom) {
-                    MSP.send_message(MSPCodes.MSP_EEPROM_WRITE, false, false, function () {
-                        GUI.log(i18n.getMessage('servosEepromSave'));
-                    });
-                }
             }
-        }
 
-        for (let servoIndex = 0; servoIndex < 8; servoIndex++) {
-            process_servo(servoIndex);
+            mspHelper.sendServoConfigurations(function () {
+                MSP.send_message(MSPCodes.MSP_EEPROM_WRITE, false, false, function () {
+                    GUI.log(i18n.getMessage('servosEepromSave'));
+                });
+            });
         }
-
-        // --- BARS ---
 
         function update_servo_bars() {
 
@@ -97,7 +163,7 @@ TABS.servos.initialize = function (callback) {
             let rangeMin, rangeMax, length, margin;
 
             for (let i = 0; i < FC.SERVO_DATA.length; i++) {
-                const servoMeter = $(`.tab-servos .servo${i} .meter`);
+                const servoMeter = $(`.tab-servos .servoConfig${i} .meter`);
                 const servoValue = FC.SERVO_DATA[i];
 
                 if (FC.SERVO_CONFIG[i].mid < 1000) {
@@ -131,29 +197,24 @@ TABS.servos.initialize = function (callback) {
             MSP.send_message(MSPCodes.MSP_STATUS);
         }
 
-        $('table.fields input[id="reversed"]').change(function() {
-            const index = $(this).data('info').index;
-            const input = $(`.tab-servos .servo${index} #trim`);
-            input.val(-parseInt(input.val()));
-        });
-
-        $('table.fields input').change(function () {
-            const index = $(this).data('info').index;
-            update_servo_config(index);
-            mspHelper.sendServoConfig(index);
-        });
 
         $('a.save').click(function () {
-            update_servo_configurations(true);
+            save_servo_configuration();
         });
 
+
+        // Initialize servos
+        for (let index = 0; index < self.MAX_SERVOS; index++) {
+            process_config(index);
+            process_override(index);
+        }
 
         // translate to user-selected language
         i18n.localizePage();
 
         // UI hooks for dynamically generated elements
         GUI.interval_add('servo_pull', update_servos, 100);
-        GUI.interval_add('status_pull', update_status, 250);
+        //GUI.interval_add('status_pull', update_status, 250);
 
         GUI.content_ready(callback);
     }
