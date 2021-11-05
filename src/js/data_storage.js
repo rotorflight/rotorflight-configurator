da6fee24481593cf1652cc1896e4dda4df45d480
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-11-05 14:57:48 +0000

    WIP: Change API version to 11.1.0

--- src/js/data_storage.js
+++ src/js/data_storage.js
@@ -13,15 +13,19 @@ const API_VERSION_1_40 = '1.40.0';
 const API_VERSION_1_41 = '1.41.0';
 const API_VERSION_1_42 = '1.42.0';
 const API_VERSION_1_43 = '1.43.0';
-const API_VERSION_1_44 = '1.44.0';
+const API_VERSION_1_44 = '255.44.0'; // 4.3.x firmware - not supported by RF
+
+const API_VERSION_RTFL_1 = '11.1.0';
+
+const API_VERSION_RTFL_MIN = '11.1.0';
+const API_VERSION_RTFL_MAX = '11.1.0';
 
 const CONFIGURATOR = {
     // all versions are specified and compared using semantic versioning http://semver.org/
-    API_VERSION_ACCEPTED: '1.2.1',
-    API_VERSION_MIN_SUPPORTED_BACKUP_RESTORE: '1.5.0',
-    API_VERSION_MIN_SUPPORTED_PID_CONTROLLER_CHANGE: '1.5.0',
-    BACKUP_FILE_VERSION_MIN_SUPPORTED: '0.55.0', // chrome.runtime.getManifest().version is stored as string, so does this one
-    API_VERSION_MAX_SUPPORTED: API_VERSION_1_44,
+    API_VERSION_MAX_SUPPORTED: API_VERSION_RTFL_MAX,
+    API_VERSION_ACCEPTED: API_VERSION_RTFL_MIN,
+    API_VERSION_MIN_SUPPORTED_BACKUP_RESTORE: API_VERSION_RTFL_MIN,
+    BACKUP_FILE_VERSION_MIN_SUPPORTED: API_VERSION_RTFL_MIN,
 
     connectionValid: false,
     connectionValidCliOnly: false,
