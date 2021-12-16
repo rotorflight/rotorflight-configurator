6d80253c4cd04bbed4387a4cfe5fc12eb52ee628
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-12-16 21:15:12 +0000

    WIP: Refactor Gyro tab

--- src/js/tabs/gyro.js
+++ src/js/tabs/gyro.js
@@ -1,14 +1,10 @@
 'use strict';
 
 TABS.gyro = {
-    dirty: false,
-    updating: true,
+    isDirty: false,
     FILTER_TYPE_NAMES: [
         "PT1",
         "BIQUAD",
-    ],
-    DYN_NOTCH_RANGE_NAMES:[
-        "HIGH", "MEDIUM", "LOW", "AUTO",
     ],
 };
 
@@ -20,19 +16,47 @@ TABS.gyro.initialize = function (callback) {
         GUI.active_tab = 'gyro';
     }
 
-    // Update filtering defaults based on API version
     const FILTER_DEFAULT = FC.getFilterDefaults();
 
-    MSP.promise(MSPCodes.MSP_STATUS)
-        .then(() => MSP.promise(MSPCodes.MSP_FEATURE_CONFIG))
-        .then(() => MSP.promise(MSPCodes.MSP_FILTER_CONFIG))
-        .then(() => load_html());
+    load_data(load_html);
 
     function load_html() {
         $('#content').load("./tabs/gyro.html", process_html);
     }
 
+    function load_data(callback) {
+        MSP.promise(MSPCodes.MSP_STATUS)
+            .then(() => MSP.promise(MSPCodes.MSP_FEATURE_CONFIG))
+            .then(() => MSP.promise(MSPCodes.MSP_FILTER_CONFIG))
+            .then(callback);
+    }
+
+    function save_data(callback) {
+        Promise.resolve(true)
+            .then(() => MSP.promise(MSPCodes.MSP_SET_FILTER_CONFIG, mspHelper.crunch(MSPCodes.MSP_SET_FILTER_CONFIG)))
+            .then(() => MSP.promise(MSPCodes.MSP_EEPROM_WRITE))
+            .then(() => {
+                GUI.log(i18n.getMessage('eepromSaved'));
+                if (callback) callback();
+            });
+    };
+
     function data_to_form() {
+
+        function populateFilterTypeSelector(name, selectValues) {
+            const filterSelect = $('select[name="' + name + '"]');
+            selectValues.forEach(function(value, key) {
+                filterSelect.append('<option value="' + key + '">' + value + '</option>');
+            });
+        }
+
+        populateFilterTypeSelector('gyroLowpassType', self.FILTER_TYPE_NAMES);
+        populateFilterTypeSelector('gyroLowpassDynType', self.FILTER_TYPE_NAMES);
+        populateFilterTypeSelector('gyroLowpass2Type', self.FILTER_TYPE_NAMES);
+
+        populateFilterTypeSelector('dtermLowpassType', self.FILTER_TYPE_NAMES);
+        populateFilterTypeSelector('dtermLowpass2Type', self.FILTER_TYPE_NAMES);
+        populateFilterTypeSelector('dtermLowpassDynType', self.FILTER_TYPE_NAMES);
 
         $('.gyro_filter select[name="gyroLowpassType"]').val(FC.FILTER_CONFIG.gyro_lowpass_type);
         $('.gyro_filter input[name="gyroLowpassFrequency"]').val(FC.FILTER_CONFIG.gyro_lowpass_hz);
@@ -58,8 +82,6 @@ TABS.gyro.initialize = function (callback) {
         $('.gyro_filter input[name="dTermNotchFrequency"]').val(FC.FILTER_CONFIG.dterm_notch_hz);
         $('.gyro_filter input[name="dTermNotchCutoff"]').val(FC.FILTER_CONFIG.dterm_notch_cutoff);
 
-        $('.gyro_filter .dynamicNotchRange').toggle(semver.lt(FC.CONFIG.apiVersion, API_VERSION_1_43));
-        $('.gyro_filter select[name="dynamicNotchRange"]').val(FC.FILTER_CONFIG.dyn_notch_range);
         $('.gyro_filter input[name="dynamicNotchWidthPercent"]').val(FC.FILTER_CONFIG.dyn_notch_width_percent);
         $('.gyro_filter input[name="dynamicNotchQ"]').val(FC.FILTER_CONFIG.dyn_notch_q);
         $('.gyro_filter input[name="dynamicNotchMinHz"]').val(FC.FILTER_CONFIG.dyn_notch_min_hz);
@@ -239,7 +261,7 @@ TABS.gyro.initialize = function (callback) {
                 }
             }
 
-            $('.gyro_filter input[name="dTermNotchFrequency"]').val(freq)
+            $('.gyro_filter input[name="dTermNotchFrequency"]').val(freq);
             $('.gyro_filter input[name="dTermNotchFrequency"]').attr('disabled', !checked).change();
             $('.gyro_filter input[name="dTermNotchCutoff"]').val(cutf);
             $('.gyro_filter input[name="dTermNotchCutoff"]').attr('disabled', !checked).change();
@@ -327,7 +349,6 @@ TABS.gyro.initialize = function (callback) {
             FC.FILTER_CONFIG.gyro_notch2_cutoff = 0;
         }
 
-        FC.FILTER_CONFIG.dyn_notch_range = parseInt($('.gyro_filter select[name="dynamicNotchRange"]').val());
         FC.FILTER_CONFIG.dyn_notch_width_percent = parseInt($('.gyro_filter input[name="dynamicNotchWidthPercent"]').val());
         FC.FILTER_CONFIG.dyn_notch_q = parseInt($('.gyro_filter input[name="dynamicNotchQ"]').val());
         FC.FILTER_CONFIG.dyn_notch_min_hz = parseInt($('.gyro_filter input[name="dynamicNotchMinHz"]').val());
@@ -371,86 +392,49 @@ TABS.gyro.initialize = function (callback) {
         // translate to user-selected language
         i18n.localizePage();
 
-        function populateFilterTypeSelector(name, selectValues) {
-            const filterSelect = $('select[name="' + name + '"]');
-            selectValues.forEach(function(value, key) {
-                filterSelect.append('<option value="' + key + '">' + value + '</option>');
-            });
-        }
-
-        populateFilterTypeSelector('gyroLowpassType', self.FILTER_TYPE_NAMES);
-        populateFilterTypeSelector('gyroLowpassDynType', self.FILTER_TYPE_NAMES);
-        populateFilterTypeSelector('gyroLowpass2Type', self.FILTER_TYPE_NAMES);
-
-        populateFilterTypeSelector('dtermLowpassType', self.FILTER_TYPE_NAMES);
-        populateFilterTypeSelector('dtermLowpass2Type', self.FILTER_TYPE_NAMES);
-        populateFilterTypeSelector('dtermLowpassDynType', self.FILTER_TYPE_NAMES);
-
-        function populateDynamicNotchRangeSelect(selectDynamicNotchRangeValues) {
-            const dynamicNotchRangeSelect = $('select[name="dynamicNotchRange"]');
-            selectDynamicNotchRangeValues.forEach(function(value, key) {
-                dynamicNotchRangeSelect.append('<option value="' + key + '">' + value + '</option>');
-            });
-        }
-
-        populateDynamicNotchRangeSelect(self.DYN_NOTCH_RANGE_NAMES);
-
+        // UI Hooks
         data_to_form();
 
+        // Hide the buttons toolbar
+        $('.tab-gyro').addClass('toolbar_hidden');
 
-        // UI Hooks
+        self.isDirty = false;
 
-        $('a.refresh').click(function () {
-            self.refresh(function () {
-                GUI.log(i18n.getMessage('gyroDataRefreshed'));
-            });
+        function setDirty() {
+            if (!self.isDirty) {
+                self.isDirty = true;
+                $('.tab-gyro').removeClass('toolbar_hidden');
+            }
+        }
+
+        $('.gyro_filter').change(function () {
+            setDirty();
         });
 
-        $('.gyro_filter').find('input').each(function (k, item) {
-            $(item).change(function () {
-                self.dirty = true;
-            });
-        });
-
-        $('a.update').click(function () {
+        self.save = function (callback) {
             form_to_data();
-            self.updating = true;
-            Promise.resolve(true).then(function () {
-                return MSP.promise(MSPCodes.MSP_SET_FILTER_CONFIG, mspHelper.crunch(MSPCodes.MSP_SET_FILTER_CONFIG));
-            }).then(function () {
-                return MSP.promise(MSPCodes.MSP_EEPROM_WRITE);
-            }).then(function () {
-                self.updating = false;
-                self.dirty = false;
-                GUI.log(i18n.getMessage('gyroEepromSaved'));
-                self.refresh();
-            });
+            save_data(callback);
+        };
+
+        self.revert = function (callback) {
+            callback();
+        };
+
+        $('a.save').click(function () {
+            self.save(() => GUI.tab_switch_reload());
         });
 
-        self.updating = false;
+        $('a.revert').click(function () {
+            self.revert(() => GUI.tab_switch_reload());
+        });
 
         GUI.content_ready(callback);
     }
-
-    function status_pull() {
-    }
-
 };
 
 
 TABS.gyro.cleanup = function (callback) {
-    const self = this;
+    this.isDirty = false;
 
     if (callback) callback();
 };
-
-TABS.gyro.refresh = function (callback) {
-    const self = this;
-
-    GUI.tab_switch_cleanup(function () {
-        self.initialize();
-        self.dirty = false;
-        if (callback) callback();
-    });
-};
-
