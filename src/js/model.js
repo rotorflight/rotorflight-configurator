62a33a53cbc399c149eeec11694cecb2ca3af381
Colin Gillespie <colin@cgillespie.xyz>
2024-10-24 00:07:27 +1000

    Update Bell 206 3D model (#188)
    * Update Bell 206 3D model with a new rotor disk, new materials and a Rotorflight branded texture
    
    * Select preview model using configured rotor direction
    
    * Improve preview model lighting
    
    Co-authored-by: Johnny31297 <135465707+Johnny31297@users.noreply.github.com>

--- src/js/model.js
+++ src/js/model.js
@@ -2,8 +2,7 @@
 
 // 3D model
 const Model = function (wrapper, canvas) {
-
-    const flip = (FC.MIXER_CONFIG.main_rotor_dir == 0);
+    const clockwise = FC.MIXER_CONFIG.main_rotor_dir === 0;
 
     this.wrapper = wrapper;
     this.canvas = canvas;
@@ -16,17 +15,11 @@ const Model = function (wrapper, canvas) {
     // Set output encoding for GLTF model format
     this.renderer.outputEncoding = THREE.sRGBEncoding;
 
-    // model file name
-    var model_file = 'fallback';
-
     // setup scene
     this.scene = new THREE.Scene();
 
     // modelWrapper adds an extra axis of rotation to avoid gimbal lock with the euler angles
     this.modelWrapper = new THREE.Object3D();
-
-    // Flip left-right if needed
-    this.scale = new THREE.Vector3(flip ? -1 : 1, 1, 1);
 
     // stationary camera
     this.camera = new THREE.PerspectiveCamera(10, this.wrapper.width() / this.wrapper.height(), 1, 10000);
@@ -35,9 +28,9 @@ const Model = function (wrapper, canvas) {
     this.camera.position.z = 800;
 
     // some light
-    const light = new THREE.AmbientLight(0x404040);
-    const light2 = new THREE.DirectionalLight(new THREE.Color(1, 1, 1), 1.5);
-    light2.position.set(0, 1, 0);
+    const light = new THREE.AmbientLight(0xffffff, 0.3);
+    const light2 = new THREE.DirectionalLight(new THREE.Color(1, 1, 1), 1.0);
+    light2.position.set(0, 600, 800);
 
     // add camera, model, light to the foreground scene
     this.scene.add(light);
@@ -46,13 +39,12 @@ const Model = function (wrapper, canvas) {
     this.scene.add(this.modelWrapper);
 
     // Load model file, add to scene and render it
-    this.loadGLTF(model_file, (function (model) {
+    this.loadGLTF(`bell_${clockwise ? 'cw' : 'ccw'}`, (model) => {
         this.model = model;
-        this.model.scale.multiply(this.scale);
         this.modelWrapper.add(model);
         this.scene.add(this.modelWrapper);
         this.render();
-    }).bind(this));
+    });
 };
 
 Model.prototype.loadGLTF = function (model_file, callback) {
