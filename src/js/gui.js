2cf2fec83bd783d4d9bab480910642db842ae48e
Chris Kim <oats87g@gmail.com>
2025-03-30 19:16:18 -0700

    Hide presets tab on mobile
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- src/js/gui.js
+++ src/js/gui.js
@@ -33,7 +33,6 @@ export const GuiControl = function () {
         'power',
         'adjustments',
         'auxiliary',
-        'presets',
         'cli',
         'configuration',
         'beepers',
