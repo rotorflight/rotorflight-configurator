adde2477a0ecf85b96b268517118da565c4f977f
Colin Gillespie <colin@cgillespie.xyz>
2025-06-09 15:52:18 +1000

    Display tooltip attributes in columns

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -5,24 +5,37 @@
 </script>
 
 <div class="container">
-  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-  {@html $i18n.t(help)}
+  {#if help}
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    <span class="message">{@html $i18n.t(help)}</span>
+  {/if}
   {#if attrs}
-    <div class="attrs">
+    <table class="attrs">
       {#each attrs as attr (attr.name)}
-        <p><b>{$i18n.t(attr.name)}:</b> {attr.value}</p>
+        <tr>
+          <th>{$i18n.t(attr.name)}</th><td>{attr.value}</td>
+        </tr>
       {/each}
-    </div>
+    </table>
   {/if}
 </div>
 
 <style lang="scss">
   .container {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 8px;
     font-size: 0.8rem;
   }
 
   .attrs {
-    margin-top: 8px;
     font-size: 0.8rem;
+  }
+
+  th {
+    text-align: left;
+    font-weight: 600;
+    padding-right: 8px;
   }
 </style>
