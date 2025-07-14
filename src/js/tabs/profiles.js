483d73f77422cf5ee0698ffccfafdf33299c7cc8
Colin Gillespie <colin@cgillespie.xyz>
2025-07-14 19:05:32 +1000

    Trigger save toolbar on gov profile change

--- src/js/tabs/profiles.js
+++ src/js/tabs/profiles.js
@@ -411,6 +411,11 @@ tab.initialize = function (callback) {
     function process_html() {
         self.svelteComponent = mount(Governor, {
             target: document.querySelector("#svelte-gov-settings"),
+            props: {
+                onchange: () => {
+                    setChanged();
+                }
+            }
         });
 
         // translate to user-selected language
