e18a12a1afba3b6389f3dc602532ea4bba2c23c2
Colin Gillespie <colin@cgillespie.xyz>
2025-07-25 14:53:05 +1000

    Revamp motors tab (#343)

---
+++ src/components/Meter.svelte
@@ -0,0 +1,89 @@
+<script>
+  let { title, leftLabel, rightLabel, value, max } = $props();
+</script>
+
+<div>
+  <div class="top-container">
+    <div class="title grow">{title}</div>
+    <div>{max}</div>
+  </div>
+  <div class="meter">
+    <div class="left-label">{leftLabel}</div>
+    <div class="right-label">{rightLabel}</div>
+    <div class="fill" style:width={`${(value ?? 0).clamp(0, 100)}%`}></div>
+  </div>
+</div>
+
+<style lang="scss">
+  *,
+  *::before,
+  *::after {
+    box-sizing: content-box;
+  }
+
+  .meter {
+    position: relative;
+    height: 20px;
+    border-radius: 2px;
+    container-type: size;
+
+    :global(html[data-theme="light"]) & {
+      color: var(--color-neutral-700);
+      background-color: var(--color-neutral-200);
+      border: 1px solid var(--color-neutral-400);
+    }
+
+    :global(html[data-theme="dark"]) & {
+      color: var(--color-neutral-100);
+      background-color: var(--color-neutral-700);
+      border: 1px solid var(--color-neutral-400);
+    }
+  }
+
+  .left-label {
+    position: absolute;
+    height: 20px;
+    line-height: 20px;
+    font-weight: 600;
+    z-index: 1;
+    left: 8px;
+  }
+
+  .right-label {
+    position: absolute;
+    height: 20px;
+    line-height: 20px;
+    font-weight: 600;
+    z-index: 1;
+    right: 8px;
+  }
+
+  .fill {
+    position: absolute;
+    height: 20px;
+    border-radius: 2px;
+    margin-left: -1px;
+    margin-top: -1px;
+    border-width: 1px;
+    border-style: solid;
+    transition: width 0.1s linear;
+
+    :global(html[data-theme="light"]) & {
+      background: hsl(var(--fill-hue, var(--val-accent-hue)), 70%, 70%);
+      border-color: hsl(var(--fill-hue, var(--val-accent-hue)), 70%, 40%);
+    }
+
+    :global(html[data-theme="dark"]) & {
+      background: hsl(var(--fill-hue, var(--val-accent-hue)), 40%, 40%);
+      border-color: hsl(var(--fill-hue, var(--val-accent-hue)), 60%, 50%);
+    }
+  }
+
+  .title {
+    font-weight: 600;
+  }
+
+  .top-container {
+    display: flex;
+  }
+</style>
---
+++ src/tabs/motors/Override/Motor.svelte
@@ -0,0 +1,235 @@
+<script>
+  import semver from "semver";
+  import { onMount, onDestroy } from "svelte";
+
+  import { FC } from "@/js/fc.svelte.js";
+  import { i18n } from "@/js/i18n.js";
+  import { API_VERSION_12_9 } from "@/js/data_storage.js";
+
+  import Section from "@/components/Section.svelte";
+  import Meter from "@/components/Meter.svelte";
+  import Slider from "@/components/Slider.svelte";
+
+  import motorState from "../state.svelte.js";
+
+  let { index } = $props();
+
+  let slider;
+
+  let width = $state(0);
+  let mobile = $derived(width <= 480);
+  let sliderDensity = $derived(mobile ? 10 : 5);
+  let sliderValues = $derived.by(() => {
+    const values = [];
+    const step = mobile ? 20 : 10;
+    for (let i = 0; i <= 100; i += step) {
+      values.push(i);
+    }
+
+    return values;
+  });
+
+  let sliderOpts = $derived({
+    range: {
+      min: 0,
+      max: 100,
+    },
+    connect: [true, false],
+    start: 0,
+    step: 1,
+    behaviour: "snap-drag",
+    pips: {
+      mode: "values",
+      values: sliderValues,
+      density: sliderDensity,
+      stepped: true,
+    },
+  });
+
+  $effect(() => {
+    slider.update(sliderOpts);
+  });
+
+  let throttle = $derived.by(() => {
+    const value = FC.MOTOR_DATA[index];
+
+    if (value < 0) {
+      return (value + 1000) / 10;
+    }
+
+    if (value > 0) {
+      return (value - 1000) / 10;
+    }
+
+    return 0;
+  });
+  let rpm = $derived(FC.MOTOR_TELEMETRY_DATA.rpm[index] ?? 0);
+  let voltage = $derived(FC.MOTOR_TELEMETRY_DATA.voltage[index] / 1000);
+  let current = $derived(FC.MOTOR_TELEMETRY_DATA.current[index] / 1000);
+  let temp1 = $derived(FC.MOTOR_TELEMETRY_DATA.temperature[index] / 10);
+  let temp2 = $derived(FC.MOTOR_TELEMETRY_DATA.temperature2[index] / 10);
+  let errors = $derived(FC.MOTOR_TELEMETRY_DATA.invalidPercent[index] / 100);
+
+  let rpmMax = $state(5000);
+  let voltageMax = $state(10);
+  let currentMax = $state(10);
+  let temp1Max = $state(100);
+  let temp2Max = $state(100);
+
+  $effect(() => {
+    if (rpm > rpmMax) {
+      rpmMax = Math.ceil((rpm + 1000) / 1000) * 1000;
+    }
+
+    if (voltage > voltageMax) {
+      voltageMax = Math.ceil((voltage + 1) / 1) * 1;
+    }
+
+    if (current > currentMax) {
+      currentMax = Math.ceil((current + 1) / 1) * 1;
+    }
+
+    if (temp1 > temp1Max) {
+      temp1Max = Math.ceil((temp1 + 1) / 1) * 1;
+    }
+
+    if (temp2 > temp2Max) {
+      temp2Max = Math.ceil((temp2 + 1) / 1) * 1;
+    }
+  });
+
+  // Limit how frequently the throttle position can be updated
+  let timeoutId;
+  function updateThrottle() {
+    if (!timeoutId) {
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+        mspHelper.sendMotorOverride(index);
+      }, 50);
+    }
+  }
+
+  let overrideIntervalId;
+  onMount(() => {
+    if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_12_9)) {
+      overrideIntervalId = setInterval(() => {
+        mspHelper.sendMotorOverride(index);
+      }, 250);
+    }
+  });
+
+  onDestroy(() => {
+    clearInterval(overrideIntervalId);
+    clearTimeout(timeoutId);
+    timeoutId = null;
+  });
+</script>
+
+<svelte:window bind:innerWidth={width} />
+
+<Section>
+  {#snippet header()}
+    <div class="header">
+      <span>Motor #{index + 1}</span>
+      <span>-</span>
+      <span>{FC.MOTOR_OVERRIDE[index] * 0.1}%</span>
+    </div>
+  {/snippet}
+  <div class="slider-container">
+    <Slider
+      bind:this={slider}
+      bind:value={
+        () => FC.MOTOR_OVERRIDE[index] * 0.1,
+        (v) => (FC.MOTOR_OVERRIDE[index] = v * 10)
+      }
+      onchange={updateThrottle}
+      opts={sliderOpts}
+    />
+  </div>
+  <div class="bars-container">
+    <Meter
+      title={$i18n.t("motorThrottle")}
+      rightLabel="100%"
+      leftLabel={`${throttle}%`}
+      value={throttle}
+    />
+    <Meter
+      title={$i18n.t("motorRPM")}
+      rightLabel={`${rpmMax.toLocaleString()} RPM`}
+      leftLabel={`${rpm.toLocaleString()} RPM`}
+      value={(100 * rpm) / rpmMax}
+    />
+    {#if motorState.telemEnabled}
+      <Meter
+        title={$i18n.t("motorVoltage")}
+        rightLabel={`${voltageMax} V`}
+        leftLabel={`${voltage.toFixed(2)} V`}
+        value={(100 * voltage) / voltageMax}
+      />
+      <Meter
+        title={$i18n.t("motorCurrent")}
+        rightLabel={`${currentMax} A`}
+        leftLabel={`${current.toFixed(2)} A`}
+        value={(100 * current) / currentMax}
+      />
+      <Meter
+        title={$i18n.t("motorTemperature", { number: 1 })}
+        rightLabel={`${temp1Max} °C`}
+        leftLabel={`${temp1.toFixed(1)} °C`}
+        value={(100 * temp1) / temp1Max}
+      />
+      <Meter
+        title={$i18n.t("motorTemperature", { number: 2 })}
+        rightLabel={`${temp2Max} °C`}
+        leftLabel={`${temp2.toFixed(1)} °C`}
+        value={(100 * temp2) / temp2Max}
+      />
+    {/if}
+    {#if FC.ESC_SENSOR_CONFIG.use_dshot_telemetry}
+      <Meter
+        title={$i18n.t("motorDshotError")}
+        rightLabel="100%"
+        leftLabel={`${errors.toFixed(2)}%`}
+        value={errors}
+      />
+    {/if}
+  </div>
+</Section>
+
+<style lang="scss">
+  .bars-container {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding: 4px;
+  }
+
+  .slider-container {
+    margin-bottom: 20px;
+    padding: 16px;
+  }
+
+  .header {
+    @extend %section-header;
+    padding-left: 8px;
+    gap: 8px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    .header {
+      margin-top: 16px;
+
+      :global(html[data-theme="light"]) & {
+        background: none;
+      }
+
+      :global(html[data-theme="dark"]) & {
+        background: none;
+      }
+    }
+
+    .slider-container {
+      margin: 20px 16px;
+    }
+  }
+</style>
